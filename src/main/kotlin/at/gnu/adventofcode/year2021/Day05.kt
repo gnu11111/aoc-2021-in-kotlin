48c1716319e36e2bbdeb1044e061514e207bc45d
gnu11111 <gnu@gnu.at>
2021-12-06 03:14:09 +0100

    polishing

--- src/main/kotlin/at/gnu/adventofcode/year2021/Day05.kt
+++ src/main/kotlin/at/gnu/adventofcode/year2021/Day05.kt
@@ -1,8 +1,99 @@
+/**
+ * ADVENT OF CODE 2021 - DAY 5: HYDROTHERMAL VENTURE
+ *
+ * You come across a field of hydrothermal vents on the ocean floor! These
+ * vents constantly produce large, opaque clouds, so it would be best to avoid
+ * them if possible.
+ *
+ * They tend to form in lines; the submarine helpfully produces a list of
+ * nearby lines of vents (your puzzle input) for you to review. For example:
+ *
+ *   0,9 -> 5,9
+ *   8,0 -> 0,8
+ *   9,4 -> 3,4
+ *   2,2 -> 2,1
+ *   7,0 -> 7,4
+ *   6,4 -> 2,0
+ *   0,9 -> 2,9
+ *   3,4 -> 1,4
+ *   0,0 -> 8,8
+ *   5,5 -> 8,2
+ *
+ * Each line of vents is given as a line segment in the format
+ * x1, y1 -> x2, y2 where x1,y1 are the coordinates of one end the line
+ * segment and x2, y2 are the coordinates of the other end. These line
+ * segments include the points at both ends. In other words:
+ *
+ *   - An entry like 1,1 -> 1,3 covers points 1,1, 1,2, and 1,3.
+ *   - An entry like 9,7 -> 7,7 covers points 9,7, 8,7, and 7,7.
+ *
+ * For now, only consider horizontal and vertical lines: lines where either
+ * x1 = x2 or y1 = y2.
+ *
+ * So, the horizontal and vertical lines from the above list would produce the
+ * following diagram:
+ *
+ *   .......1..
+ *   ..1....1..
+ *   ..1....1..
+ *   .......1..
+ *   .112111211
+ *   ..........
+ *   ..........
+ *   ..........
+ *   ..........
+ *   222111....
+ *
+ * In this diagram, the top left corner is 0,0 and the bottom right corner is
+ * 9, 9. Each position is shown as the number of lines which cover that point
+ * or . if no line covers that point. The top-left pair of 1s, for example,
+ * comes from 2, 2 -> 2, 1; the very bottom row is formed by the overlapping
+ * lines 0, 9 -> 5, 9 and 0, 9 -> 2, 9.
+ *
+ * To avoid the most dangerous areas, you need to determine the number of
+ * points where at least two lines overlap. In the above example, this is
+ * anywhere in the diagram with a 2 or larger - a total of 5 points.
+ *
+ * Consider only horizontal and vertical lines. At how many points do at least
+ * two lines overlap?
+ *
+ * --- Part Two ---
+ *
+ * Unfortunately, considering only horizontal and vertical lines doesn't give
+ * you the full picture; you need to also consider diagonal lines.
+ *
+ * Because of the limits of the hydrothermal vent mapping system, the lines in
+ * your list will only ever be horizontal, vertical, or a diagonal line at
+ * exactly 45 degrees. In other words:
+ *
+ * An entry like 1,1 -> 3,3 covers points 1,1, 2,2, and 3,3.
+ * An entry like 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+ *
+ * Considering all lines from the above example would now produce the
+ * following diagram:
+ *
+ *   1.1....11.
+ *   .111...2..
+ *   ..2.1.111.
+ *   ...1.2.2..
+ *   .112313211
+ *   ...1.2....
+ *   ..1...1...
+ *   .1.....1..
+ *   1.......1.
+ *   222111....
+ *
+ * You still need to determine the number of points where at least two lines
+ * overlap. In the above example, this is still anywhere in the diagram with
+ * a 2 or larger - now a total of 12 points.
+ *
+ * Consider all of the lines. At how many points do at least two lines
+ * overlap?
+ */
 package at.gnu.adventofcode.year2021
 
 import kotlin.math.abs
 import kotlin.math.max
-import kotlin.math.min
 
 class Day05(input: List<String>) {
 
@@ -12,88 +103,38 @@ class Day05(input: List<String>) {
 
     data class Coordinate(val x: Int, val y: Int)
     data class Line(val from: Coordinate, val to: Coordinate)
+
     private val lines: List<Line>
+
     init {
-        val newLines = mutableListOf<Line>()
+        val linesFromInput = mutableListOf<Line>()
         for (line in input) {
-            val (from, to) = line.split("->").map { it.split(",") }
-                .map { Coordinate(it[0].trim().toInt(), it[1].trim().toInt()) }
-            newLines.add(Line(from, to))
+            val (from, to) = line.split(" -> ").map { it.split(",") }.map { Coordinate(it[0].toInt(), it[1].toInt()) }
+            linesFromInput.add(Line(from, to))
         }
-        lines = newLines.toList()
+        lines = linesFromInput.toList()
     }
 
 
-    fun part1(): Int {
-        val coordinate = mutableMapOf<Coordinate, Int>()
-        for (line in lines) {
-            if ((line.from.x != line.to.x) && (line.from.y != line.to.y))
-                continue
-            if (line.from.x == line.to.x)
-                for (y in min(line.from.y, line.to.y)..max(line.from.y, line.to.y)) {
-                    val newCoordinate = Coordinate(line.from.x, y)
-                    if (!coordinate.containsKey(newCoordinate))
-                        coordinate[newCoordinate] = 1
-                    else
-                        coordinate[newCoordinate] = coordinate[newCoordinate]!! + 1
-                }
-            else
-                for (x in min(line.from.x, line.to.x)..max(line.from.x, line.to.x)) {
-                    val newCoordinate = Coordinate(x, line.from.y)
-                    if (!coordinate.containsKey(newCoordinate))
-                        coordinate[newCoordinate] = 1
-                    else
-                        coordinate[newCoordinate] = coordinate[newCoordinate]!! + 1
-                }
-        }
-        return coordinate.values.count { it > 1 }
-    }
+    fun part1(): Int =
+        calculateCoordinates()
 
-    fun part2(): Int {
+    fun part2(): Int =
+        calculateCoordinates(withDiagonals = true)
+
+    private fun calculateCoordinates(withDiagonals: Boolean = false): Int {
         val coordinates = mutableMapOf<Coordinate, Int>()
         for (line in lines) {
-            if (line.from.x == line.to.x)
-                for (y in min(line.from.y, line.to.y)..max(line.from.y, line.to.y)) {
-                    val newCoordinate = Coordinate(line.from.x, y)
-                    if (!coordinates.containsKey(newCoordinate))
-                        coordinates[newCoordinate] = 1
-                    else
-                        coordinates[newCoordinate] = coordinates[newCoordinate]!! + 1
-                }
-            else if (line.from.y == line.to.y)
-                for (x in min(line.from.x, line.to.x)..max(line.from.x, line.to.x)) {
-                    val newCoordinate = Coordinate(x, line.from.y)
-                    if (!coordinates.containsKey(newCoordinate))
-                        coordinates[newCoordinate] = 1
-                    else
-                        coordinates[newCoordinate] = coordinates[newCoordinate]!! + 1
-                }
-            else
-                for (i in 0..abs(line.from.x - line.to.x)) {
-                    val dx = if (line.to.x > line.from.x) i else -i
-                    val dy = if (line.to.y > line.from.y) i else -i
-                    val newCoordinate = Coordinate(line.from.x + dx, line.from.y + dy)
-                    if (!coordinates.containsKey(newCoordinate))
-                        coordinates[newCoordinate] = 1
-                    else
-                        coordinates[newCoordinate] = coordinates[newCoordinate]!! + 1
-                }
+            if (!withDiagonals && (line.from.x != line.to.x) && (line.from.y != line.to.y))
+                continue
+            for (i in 0..max(abs(line.from.x - line.to.x), abs(line.from.y - line.to.y))) {
+                val dx = line.to.x.compareTo(line.from.x) * i
+                val dy = line.to.y.compareTo(line.from.y) * i
+                val newCoordinate = Coordinate(line.from.x + dx, line.from.y + dy)
+                coordinates[newCoordinate] = (coordinates[newCoordinate] ?: 0) + 1
+            }
         }
         return coordinates.values.count { it > 1 }
-    }
-
-    @Suppress("unused")
-    private fun showMap(coordinates: Map<Coordinate, Int>) {
-        for (y in 0..9) {
-            val line = StringBuilder()
-            for (x in 0..9) {
-                if (coordinates.containsKey(Coordinate(x, y)))
-                    line.append(coordinates[Coordinate(x, y)])
-                else
-                    line.append(".")
-            }
-            println(line)
-        }
     }
 }
 
