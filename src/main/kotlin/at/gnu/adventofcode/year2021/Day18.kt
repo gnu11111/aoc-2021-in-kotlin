a5302663678dd10af79e2793f354fff063d4ec31
gnu11111 <gnu@gnu.at>
2021-12-20 00:02:14 +0100

    Day 18 - Snailfish

---
+++ src/main/kotlin/at/gnu/adventofcode/year2021/Day18.kt
@@ -0,0 +1,131 @@
+package at.gnu.adventofcode.year2021
+
+class Day18(private val numbers: List<String>) {
+
+    companion object {
+        const val input = "/adventofcode/year2021/Day18.txt"
+    }
+
+    fun part1(): Int =
+        numbers.fold("") { acc, number -> acc.add(number.reduceNumber()).reduceNumber() }.calculateMagnitude()
+
+    fun part2(): Int =
+        numbers.indices.map { i ->
+            numbers.indices.map { j ->
+                if (i != j)
+                    "".add(numbers[i].reduceNumber()).add(numbers[j].reduceNumber()).reduceNumber().calculateMagnitude()
+                else
+                    0
+            }
+        }.flatten().maxOf { it }
+
+    private fun String.add(number: String): String =
+        if (this.isBlank()) number else "[$this,$number]"
+
+    private fun String.reduceNumber(): String {
+        var number = this
+        var brackets = 0
+        var i = -1
+        while (++i < (number.length - 1)) {
+            if (number[i] == '[') {
+                if (++brackets > 4) {
+                    val newNumber = number.explode(i)
+                    if (newNumber != number) {
+                        number = newNumber
+//                      println("after explode: $number")
+                    }
+                    i = -1
+                    brackets = 0
+                }
+            } else if (number[i] == ']')
+                --brackets
+        }
+        val newSplitNumber = number.splitNumber()
+        if (newSplitNumber != number) {
+            number = newSplitNumber
+//          println("after split:   $number")
+            return number.reduceNumber()
+        }
+        return number
+    }
+
+    private fun String.explode(start: Int): String {
+        val end = start + this.substring(start).indexOf(']')
+        val snailFishNumber = """\[(\d+),(\d+)""".toRegex()
+        val (first, second) = snailFishNumber.matchEntire(this.substring(start, end))!!.destructured
+        val left = this.substring(0, start).addToLastNumber(first)
+        val right = this.substring(end + 1).addToFirstNumber(second)
+        return left + "0" + right
+    }
+
+    private fun String.splitNumber(): String {
+        var start = -1
+        for (i in 0 until this.length) {
+            if ((this[i] in '0'..'9') && (start < 0))
+                start = i
+            else if ((this[i] !in '0'..'9') && (start >= 0)) {
+                val number = this.substring(start, i).toInt()
+                if (number >= 10)
+                    return this.substring(0, start) + "[" + (number / 2) + "," + ((number + 1) / 2) + "]" +
+                            this.substring(i)
+                start = -1
+            }
+        }
+        return this
+    }
+
+    private fun String.addToLastNumber(number: String): String {
+        var start = -1
+        var end = -1
+        for (i in (this.length - 1) downTo 0) {
+            if ((this[i] in '0'..'9') && (end < 0))
+                end = i + 1
+            else if ((this[i] !in '0'..'9') && (end >= 0)) {
+                start = i + 1
+                break
+            }
+        }
+        return if (start < 0)
+            this
+        else {
+            this.substring(0, start) + (this.substring(start, end).toInt() + number.toInt()) + this.substring(end)
+        }
+    }
+
+    private fun String.addToFirstNumber(number: String): String =
+        this.replace("""(\D*?)(\d+)(.*)""".toRegex()) {
+            it.groupValues[1] + (it.groupValues[2].toInt() + number.toInt()) + it.groupValues[3]
+        }
+
+    private fun String.calculateMagnitude(): Int {
+        var brackets = 0
+        var i = -1
+        var max = -1
+        var start = -1
+        while (++i < (this.length - 1)) {
+            if (this[i] == '[') {
+                if (++brackets > max) {
+                    max = brackets
+                    start = i
+                }
+            } else if (this[i] == ']')
+                --brackets
+        }
+        if (start >= 0) {
+            val end = start + this.substring(start).indexOf("]")
+            val numbers = this.substring(start + 1, end).split(",").map { it.toInt() }
+//          println("magnitude: ${this.substring(0, start) + ((numbers.first() * 3) + (numbers.last() * 2)) +
+//                  this.substring(end + 1)}")
+            return (this.substring(0, start) + ((numbers.first() * 3) + (numbers.last() * 2)) +
+                    this.substring(end + 1)).calculateMagnitude()
+        }
+        return this.toInt()
+    }
+}
+
+fun main() {
+    val numbers = Day18::class.java.getResource(Day18.input)!!.readText().trim().split("\n", "\r\n")
+    val day18 = Day18(numbers)
+    println("Day18::part1 -> ${day18.part1()}")
+    println("Day18::part2 -> ${day18.part2()}")
+}
---
+++ src/test/kotlin/at/gnu/adventofcode/year2021/Day18Test.kt
@@ -0,0 +1,37 @@
+package at.gnu.adventofcode.year2021
+
+import org.junit.Assert
+import org.junit.Test
+import kotlin.system.measureNanoTime
+
+class Day18Test {
+
+    private val shellfishNumbers = """
+        [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+        [[[5,[2,8]],4],[5,[[9,9],0]]]
+        [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+        [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+        [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+        [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+        [[[[5,4],[7,7]],8],[[8,3],8]]
+        [[9,3],[[9,9],[6,[4,9]]]]
+        [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+        [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+    """.trimIndent().split("\n")
+
+    private val test = mapOf(
+        Day18::part1 to 4140,
+        Day18::part2 to 3993
+    )
+
+    @Test
+    fun testMySolution() {
+        val day18 = Day18(shellfishNumbers)
+        for (function in test.keys) {
+            val result: Int
+            val time = measureNanoTime { result = function(day18) }
+            println("Day18::${function.name}: ${shellfishNumbers.size} shellfish-numbers -> $result [${time}ns]")
+            Assert.assertEquals(test[function], result)
+        }
+    }
+}
