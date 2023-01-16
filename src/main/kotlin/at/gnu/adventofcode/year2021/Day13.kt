3bfd6fd53943ca52ebbecfa45db37a14782a6b6a
gnu11111 <gnu@gnu.at>
2023-01-16 14:09:59 +0100

    polishing

--- src/main/kotlin/at/gnu/adventofcode/year2021/Day13.kt
+++ src/main/kotlin/at/gnu/adventofcode/year2021/Day13.kt
@@ -1,37 +1,37 @@
 package at.gnu.adventofcode.year2021
 
-class Day13(dotsFromInput: List<String>, foldersFromInput: List<String>) {
+class Day13(dotsFromInput: List<String>, foldingsFromInput: List<String>) {
 
     companion object {
-        const val input = "/adventofcode/year2021/Day13.txt"
+        const val resource = "/adventofcode/year2021/Day13.txt"
         const val verticalFolderType = "x"
-        private val folderCommand = """fold along ([xy])=(\d+)""".toRegex()
+        private val foldingCommand = """fold along ([xy])=(\d+)""".toRegex()
         private val outOfBounds = Dot(-1, -1)
     }
 
     data class Dot(val x: Int, val y: Int)
-    sealed class Folder(val transform: (Dot) -> Dot)
+    sealed class Folding(val transform: (Dot) -> Dot)
 
     private val dots = dotsFromInput.map { it.split(",") }.map { Dot(it[0].toInt(), it[1].toInt()) }.toSet()
-    private val folders = foldersFromInput.map {
-        val (type, amount) = folderCommand.matchEntire(it)!!.destructured
-        if (type == verticalFolderType) VerticalFolder(amount.toInt()) else HorizontalFolder(amount.toInt())
+    private val foldings = foldingsFromInput.map {
+        val (type, amount) = foldingCommand.matchEntire(it)!!.destructured
+        if (type == verticalFolderType) VerticalFolding(amount.toInt()) else HorizontalFolding(amount.toInt())
     }
 
     fun part1(): Int =
-        dots.creasePaperUsing(folders.first()).size
+        dots.creasePaperUsing(foldings.first()).size
 
     fun part2(): Int =
-        folders.fold(dots) { newDots, folder -> newDots.creasePaperUsing(folder) }.also { prettyPrint(it) }.size
+        foldings.fold(dots) { newDots, folding -> newDots.creasePaperUsing(folding) }.also { prettyPrint(it) }.size
 
-    private fun Set<Dot>.creasePaperUsing(folder: Folder): Set<Dot> =
-        this.map { dot -> folder.transform(dot) }.toSet()
+    private fun Set<Dot>.creasePaperUsing(folding: Folding): Set<Dot> =
+        this.map { dot -> folding.transform(dot) }.toSet()
 
-    class HorizontalFolder(y: Int) : Folder({ dot ->
+    class HorizontalFolding(y: Int) : Folding({ dot ->
         if (dot.y < y) dot else if (dot.y > y) Dot(dot.x, (2 * y) - dot.y) else outOfBounds
     })
 
-    class VerticalFolder(x: Int) : Folder({ dot ->
+    class VerticalFolding(x: Int) : Folding({ dot ->
         if (dot.x < x) dot else if (dot.x > x) Dot((2 * x) - dot.x, dot.y) else outOfBounds
     })
 
@@ -43,9 +43,9 @@ class Day13(dotsFromInput: List<String>, foldersFromInput: List<String>) {
 }
 
 fun main() {
-    val (dots, folders) = Day13::class.java.getResource(Day13.input)!!.readText().trim()
+    val (dots, foldings) = Day13::class.java.getResource(Day13.resource)!!.readText().trim()
         .split("\n\n", "\r\n\r\n").map { it.split("\n", "\r\n") }
-    val day13 = Day13(dots, folders)
+    val day13 = Day13(dots, foldings)
     println("Day13::part1 -> ${day13.part1()}")
     println("Day13::part2 -> ${day13.part2()}")
 }
